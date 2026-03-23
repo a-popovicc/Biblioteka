4fd0b5a8c9b0834a6410ee89763d580dfd375283
a-popovicc <apopoviicc@gmail.com>
2026-03-23 02:40:51 +0100

    dodata klasa stamparija

---
+++ src/biblioteka/Stamparija.java
@@ -0,0 +1,31 @@
+package biblioteka;
+
+public class Stamparija {
+    private int id;
+    private String name;
+    private String location;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
